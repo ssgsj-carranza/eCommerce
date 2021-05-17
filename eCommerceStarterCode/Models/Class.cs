86bf50a64a3744e295656f8b9aeec3d159f89e90
Eric Lars Lee <ericlarslee@gmail.com>
2021-05-17 10:59:30 -0500

    added Product class with name, description, and price properties

---
+++ eCommerceStarterCode/Models/Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Models
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
