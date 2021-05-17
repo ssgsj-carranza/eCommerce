246a45a3a0547faf971e8210900c27320d3c66ab
unknown <ssgsj.carranza@gmail.com>
2021-05-17 11:27:45 -0500

    added product controller and model, shopping cart controller and model

---
+++ eCommerceStarterCode/Controllers/ShoppingCartController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerceStarterCode.Data;
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingCartController : ControllerBase
+    {
+        public ApplicationDbContext _context;
+        public ShoppingCartController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ eCommerceStarterCode/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+    }
+}
