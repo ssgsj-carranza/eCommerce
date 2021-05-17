b59c24daeabead2b2f8f0cd04c0fa575fc5997c9
unknown <ssgsj.carranza@gmail.com>
2021-05-17 11:52:51 -0500

    pushing codes for products and models

--- eCommerceStarterCode/Models/Product.cs
+++ eCommerceStarterCode/Models/Product.cs
@@ -7,6 +7,10 @@ namespace eCommerceStarterCode.Models
 {
     public class Product
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string  Description{ get; set; }
+        public virtual ICollection<ShoppingCart> ShoppingCarts { get; set; }
     }
 }
--- eCommerceStarterCode/Models/ShoppingCart.cs
+++ eCommerceStarterCode/Models/ShoppingCart.cs
@@ -7,5 +7,11 @@ namespace eCommerceStarterCode.Models
 {
     public class ShoppingCart
     {
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+        public int? Quantity { get; set; }
+
+        public virtual Product Product { get; set; }
+        public virtual User User { get; set; }
     }
 }
