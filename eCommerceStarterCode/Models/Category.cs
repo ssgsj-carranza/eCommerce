4e82d4b4025fd08a566892f53bd7aa9d8df34e6e
unknown <ssgsj.carranza@gmail.com>
2021-05-18 11:04:10 -0500

    added category model

--- eCommerceStarterCode/Models/Category.cs
+++ eCommerceStarterCode/Models/Category.cs
@@ -7,16 +7,12 @@ using System.Threading.Tasks;
 
 namespace eCommerceStarterCode.Models
 {
-    public class Product
+    public class Category
     {
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
-        public decimal Price { get; set; }
-        public string  Description{ get; set; }
-        
-        [ForeignKey("Category")]
-        public int CategoryId { get; set; }
-        public Category Category { get; set; }
+
     }
+
 }
