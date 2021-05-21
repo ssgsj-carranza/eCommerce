47c2b4ddf5f6082d6a7eb395c7b98f8333cfc67b
unknown <ssgsj.carranza@gmail.com>
2021-05-21 09:29:02 -0500

    fixed merger

--- eCommerceStarterCode/Migrations/20210521075823_reviewsTable.cs
+++ eCommerceStarterCode/Migrations/20210521075823_reviewsTable.cs
@@ -34,25 +34,25 @@ namespace eCommerceStarterCode.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
-            migrationBuilder.InsertData(
-                table: "AspNetRoles",
-                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
-                values: new object[] { "513c28d8-3ea1-4ebf-9d34-fb4e56c49be9", "8ff5a7d5-cc9e-4c15-a310-d7cf00979ab6", "User", "USER" });
+            //migrationBuilder.InsertData(
+            //    table: "AspNetRoles",
+            //    columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+            //    values: new object[] { "513c28d8-3ea1-4ebf-9d34-fb4e56c49be9", "8ff5a7d5-cc9e-4c15-a310-d7cf00979ab6", "User", "USER" });
 
-            migrationBuilder.InsertData(
-                table: "AspNetRoles",
-                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
-                values: new object[] { "1c6e07f0-d37f-4a01-b7dc-ea2727041849", "b0132326-564a-4220-842b-34f08e400947", "Admin", "ADMIN" });
+            //migrationBuilder.InsertData(
+            //    table: "AspNetRoles",
+            //    columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+            //    values: new object[] { "1c6e07f0-d37f-4a01-b7dc-ea2727041849", "b0132326-564a-4220-842b-34f08e400947", "Admin", "ADMIN" });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Reviews_ProductId",
-                table: "Reviews",
-                column: "ProductId");
+            //migrationBuilder.CreateIndex(
+            //    name: "IX_Reviews_ProductId",
+            //    table: "Reviews",
+            //    column: "ProductId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Reviews_UserId",
-                table: "Reviews",
-                column: "UserId");
+            //migrationBuilder.CreateIndex(
+            //    name: "IX_Reviews_UserId",
+            //    table: "Reviews",
+            //    column: "UserId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
